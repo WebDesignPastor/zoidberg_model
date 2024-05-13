67de2db1fbf9d119e04484970061e81f5c9985a0
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2024-05-13 15:26:14 +0200

    update size images

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 from network import CNN
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+     transforms.Resize((224)), # resize doc: If size is an int, smaller edge of the image will be matched to this number.
+    transforms.CenterCrop((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
